0cc1ffe7a81e93f0636211d8d2b53e6eaa79d238
orlion312 <orlion312@gmail.com>
2017-11-29 00:32:00 +0200

    finished board test and updated logic test

--- tests/StdLogic_test.cpp
+++ tests/StdLogic_test.cpp
@@ -4,7 +4,65 @@
 
 #include "StdLogic_test.h"
 
-TEST_F(StdLogic_test, StartingPositionOfPossibleMoves) {
+TEST_F(StdLogic_test, StartingPositionOfPossibleMovesHuman) {
+  moves = logic->getPossibleMoves(*blackPlayer, bo);
+  EXPECT_FALSE(moves.empty());
+  EXPECT_EQ(4 , moves.size());
+  EXPECT_EQ(moves[0]->toCoordinate(), "(3,4)");
+  EXPECT_EQ(moves[1]->toCoordinate(), "(4,3)");
+  EXPECT_EQ(moves[2]->toCoordinate(), "(5,6)");
+  EXPECT_EQ(moves[3]->toCoordinate(), "(6,5)");
+  moves = logic->getPossibleMoves(*whitePlayer, bo);
+  EXPECT_FALSE(moves.empty());
+  EXPECT_EQ(4 , moves.size());
+  EXPECT_EQ(moves[0]->toCoordinate(), "(3,5)");
+  EXPECT_EQ(moves[1]->toCoordinate(), "(4,6)");
+  EXPECT_EQ(moves[2]->toCoordinate(), "(5,3)");
+  EXPECT_EQ(moves[3]->toCoordinate(), "(6,4)");
+}
 
+TEST_F(StdLogic_test, MidGameCheck) {
+  bo.getCell(4,4)->setDisk('X');
+  bo.getCell(4,5)->setDisk('X');
+  bo.getCell(5,4)->setDisk('X');
+  bo.getCell(5,6)->setDisk('X');
+  bo.getCell(5,5)->setDisk('O');
+  bo.getCell(6,6)->setDisk('O');
+  moves = logic->getPossibleMoves(*blackPlayer, bo);
+  EXPECT_EQ(moves[0]->toCoordinate(), "(3,3)");
+  EXPECT_EQ(moves[1]->toCoordinate(), "(3,4)");
+  EXPECT_EQ(moves[2]->toCoordinate(), "(4,3)");
+  EXPECT_EQ(moves[3]->toCoordinate(), "(7,6)");
+  EXPECT_EQ(moves[4]->toCoordinate(), "(8,7)");
+  moves = logic->getPossibleMoves(*whitePlayer, bo);
+  EXPECT_EQ(moves[0]->toCoordinate(), "(4,6)");
+  EXPECT_EQ(moves[1]->toCoordinate(), "(5,7)");
+  EXPECT_EQ(moves[2]->toCoordinate(), "(6,4)");
+  EXPECT_EQ(moves[3]->toCoordinate(), "(6,8)");
+  bo.getCell(3,5)->setDisk('O');
+  cell = bo.getCell(3,5);
+  logic->flip(*whitePlayer,*cell ,bo);
+  EXPECT_EQ('O', bo.getCell(3,4)->getDisk());
+  EXPECT_EQ('O', bo.getCell(4,5)->getDisk());
+}
 
+TEST_F(StdLogic_test, NoMoves) {
+  bo.clearBoard();
+  moves = logic->getPossibleMoves(*blackPlayer, bo);
+  EXPECT_TRUE(moves.empty());
+  moves = logic->getPossibleMoves(*whitePlayer, bo);
+  EXPECT_TRUE(moves.empty());
+
+}
+
+TEST_F(StdLogic_test, FullBoard) {
+  for(int i = 0; i < bo.getSize();i++) {
+    for(int j = 0; j < bo.getSize();j++) {
+      bo.getCell(i,j)->setDisk(Globals::kWhites);
+    }
+  }
+  moves = logic->getPossibleMoves(*blackPlayer, bo);
+  EXPECT_TRUE(moves.empty());
+  moves = logic->getPossibleMoves(*whitePlayer, bo);
+  EXPECT_TRUE(moves.empty());
 }
--- tests/StdLogic_test.h
+++ tests/StdLogic_test.h
@@ -7,18 +7,26 @@
 #include "../src/Board.h"
 #include "../src/HumanPlayer.h"
 #include "../src/ConsoleDisplay.h"
+#include "../src/CompPlayer.h"
 
 #ifndef REVERSI_STDLOGIC_TEST_H
 #define REVERSI_STDLOGIC_TEST_H
 
 struct StdLogic_test : testing::Test {
-  Board board;
+  Cell* cell;
+  Board* board;
+  Board bo;
   Player* blackPlayer;
   Player* whitePlayer;
+  Player* CompWhitePlayer;
   Logic* logic;
   Display* display;
+  vector<Cell *> moves;
   StdLogic_test() {
+      board = &bo;
+
     display = new ConsoleDisplay();
+    CompWhitePlayer = new CompPlayer(display, board , logic, Globals::kWhites);
     blackPlayer = new HumanPlayer(display,Globals::kBlacks);
     whitePlayer = new HumanPlayer(display, Globals::kWhites);
     logic = new StdLogic();
